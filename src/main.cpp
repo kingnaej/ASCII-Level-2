79ef25c534a806ae5e8cc2012db8854add8c1906
Naej <naej0767493456@gmail.com>
2026-03-30 06:02:34 +0000

    test pour fichier main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,46 +6,6 @@
 
 int main()
 {
-    if (std::filesystem::path const dataPath = "../../data"; std::filesystem::exists(dataPath))
-    {
-        std::unordered_map<char, std::filesystem::path> asciiFile;
-        asciiFile[' '] = "../../data/espace.txt";
-        for (auto const &folder : std::filesystem::directory_iterator(dataPath))
-        {
-            if (std::filesystem::is_directory(folder))
-            {
-                for (const auto &file : std::filesystem::directory_iterator(folder))
-                {
-                    if (!std::filesystem::is_directory(file) == true && std::filesystem::path(file).extension() == ".txt")
-                    {
-                        asciiFile[file.path().stem().string()[0]] = file.path();
-                    }
-                }
-            }
-        }
-
-        std::string firstTest;
-        std::cout << "Ecrivez quelque chose (uniquement lettres et chiffres)\nEvitez les caracteres speciaux et les symboles (+, -, @, &, etc.) : ";
-        std::getline(std::cin, firstTest);
-        std::cout << std::endl;
-
-        for (auto &content : firstTest) {
-            content = static_cast<char>(std::toupper(static_cast<unsigned char>(content)));
-            if (asciiFile.contains(content)) {
-                if (std::ifstream testFile = asciiFile.at(content); testFile.is_open()) {
-                    std::string line;
-                    while (std::getline(testFile, line)) {
-                        std::cout << line << std::endl;
-                    }
-                    std::cout << std::endl;
-                }
-            }
-        }
-    }
-    else
-    {
-        std::cout << "The path: " << dataPath << " does not exist." << std::endl;
-    }
-
+    std::cout << "Hello LEVEL 2 OF ASCCI PROJECT !!" << std::endl;
     return 0;
 }
