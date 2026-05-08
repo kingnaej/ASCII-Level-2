a49027880bfdd389bfea629c80565093254c8654
Naej <naej0767493456@gmail.com>
2026-05-08 00:19:40 +0000

    Ajout final

---
+++ Readme.md
@@ -0,0 +1,40 @@
+## QU'EST-CE QUE ASCII LEVEL 2 ?
+
+ASCII LEVEL 2 EST UNE APP QUI CONSISTE A CREER UN ART ASCII A PARMIS D'IMAGES CONTENU DANS LE DOSSIER ASSETS DU PROJETS.
+
+L'IMAGE SERA SELCTIONNE ALEATOIREMENT !
+
+N.B : VOUS POUVEZ AJOUTER VOS IMAGES DANS LE DOSSIER 'ASSETS' POUR VOIR VOS IMAGES EN FORMAT ASCII
+
+
+### 1. Prérequis
+* Un compilateur C++ (GCC, Clang ou MSVC)
+* CMake 3.20 ou plus
+
+### 2. Compilation
+
+* 1- Cloner le projet : git clone https://github.com/kingnaej/ASCII-Level-2.git
+* 2- cd ASCII-Level-2
+
+* 3- 
+
+    #### Pour Windows avec Visual Studio, Linux ou MAC :
+        cmake -S . -B build
+    #### Pour Windows avec MinGW :
+	    cmake -S . -B build -G "MinGW Makefiles"
+
+* 4- cmake --build build
+
+### 3. DISTRIBUTION
+
+    cmake --install build --prefix ./dist
+
+### 4. LANCER L'APP
+
+    # Pour Windows
+
+		.\dist\bin\SchoolTerminal.exe
+
+	# Linux / macOS
+
+		./dist/bin/SchoolTerminal
--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,26 @@
 int main(int argc, char* argv[])
 {
 
-    std::string const folderPath = "assets";
+    std::filesystem::path const exeDir = std::filesystem::absolute(argv[0]);
+    // 1. On définit le chemin du dossier data (juste à côté de l'exe)
+    std::filesystem::path dataPath = exeDir.parent_path() / "assets";
+
+    // 2. Optionnel : Si on est en mode "développement" (dans build/src)
+    // On vérifie si data n'est pas un cran au-dessus (à la racine du build)
+    if (!std::filesystem::exists(dataPath) && std::filesystem::exists(exeDir.parent_path() / "data")) {
+        dataPath = exeDir.parent_path() / "assets";
+    }
+
+    // 3. Sécurité : Créer le dossier s'il n'existe nulle part
+    if (!std::filesystem::exists(dataPath)) {
+        std::filesyste
+        m::create_directories(dataPath);
+    }
+
     std::map<int, std::filesystem::path> files;
-    if (std::filesystem::exists(folderPath)) {
+    if (std::filesystem::exists(dataPath)) {
         int file_count = 0;
-        for (auto const &file : std::filesystem::directory_iterator(folderPath)) {
+        for (auto const &file : std::filesystem::directory_iterator(dataPath)) {
             if (std::filesystem::is_regular_file(file)) {
                 std::string ext = file.path().extension().string();
 
